d61ea281365f56f16aa24bcb26f00a207d600977
thallystorres <thallys.auugusto.torres@gmail.com>
2025-12-04 00:30:53 -0300

    Criação dos endpoints de API de alunos, cursos e matrículas

--- djangoapp/core/views.py
+++ djangoapp/core/views.py
@@ -5,20 +5,31 @@ from .serializers import AlunoSerializer, CursoSerializer, MatriculaSerializer
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
+
 class AlunoViewSet(viewsets.ModelViewSet):
     queryset = Aluno.objects.all()
     serializer_class = AlunoSerializer
+
 
 class CursoViewSet(viewsets.ModelViewSet):
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
 
-    @action(detail=False, methods=['get'], url_path='por-aluno/(?P<aluno_id>[^/.]+)')
+
+class MatriculaViewSet(viewsets.ModelViewSet):
+    queryset = Matricula.objects.all()
+    serializer_class = MatriculaSerializer
+
+    @action(detail=False, methods=["get"], url_path="por-aluno/(?P<aluno_id>[^/.]+)")
     def listar_por_aluno(self, request, aluno_id=None):
         matriculas = Matricula.objects.filter(aluno_id=aluno_id)
         serializer = MatriculaSerializer(matriculas, many=True)
         return Response(serializer.data)
 
-class MatriculaViewSet(viewsets.ModelViewSet):
-    queryset = Matricula.objects.all()
-    serializer_class = MatriculaSerializer
+    @action(detail=True, methods=["post"])
+    def marcar_como_paga(self, request, pk=None):
+        matricula = self.get_object()
+        matricula.status = "pago"
+        matricula.save()
+        serializer = MatriculaSerializer(matricula)
+        return Response(serializer.data)
--- djangoapp/project/urls.py
+++ djangoapp/project/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from core.views import AlunoViewSet, CursoViewSet, MatriculaViewSet
+
+router = DefaultRouter()
+router.register(r'alunos', AlunoViewSet)
+router.register(r'cursos', CursoViewSet)
+router.register(r'matriculas', MatriculaViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls))
 ]
